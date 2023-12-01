41176e842c97c17037496d93c28bfc57f9af8a49
Akinfolarin <akin.ogunmodede@gmail.com>
2023-12-01 16:44:35 -0500

    Added RequestGroup to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import List from './List.js';
 import UserProfile from './components/UserProfile';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RequestGroups from './components/RequestGroup.js';
+import RequestGroup from './components/RequestGroup.js';
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<List />} />
           <Route path="/userprofile" element={<UserProfile />} />
+          <Route path="/requestgroup" element={<RequestGroup />} />
         </Routes>
       </div>
     </Router>
