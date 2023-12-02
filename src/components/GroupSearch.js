03768100c49cd938879d04c3a59a58e0e694062c
Lara Karacasu <larakaracasu@gmail.com>
2023-12-01 21:47:45 -0500

    Added mock profile pictures to Group Search.

--- src/components/GroupSearch.js
+++ src/components/GroupSearch.js
@@ -1,4 +1,5 @@
 import './GroupSearch.css';
+import lionImg from "../images/lion.jpg"
 import React from "react";
 
 function GroupSearch(){
@@ -11,10 +12,10 @@ function GroupSearch(){
                 <input class="form-input" type="text" id="name" placeholder="Search Group"></input>
             </form>
             <div className = "List">
-                <ListItem  index={1} icon="X" groupName="Group 1" description= "90% match" memberNum= "Members:  2 of 5"></ListItem>
-                <ListItem  index={2} icon="X" groupName="Group 2" description= "58% match" memberNum= "Members:  2 of 5"></ListItem>
-                <ListItem  index={3} icon="X" groupName="Group 3" description= "25% match" memberNum= "Members:  2 of 5"></ListItem>
-                <ListItem  index={4} icon="X" groupName="Group 4" description= "15% match" memberNum= "Members:  2 of 5"></ListItem>
+                <ListItem  index={1} image={lionImg} groupName="Group 1" description= "90% match" memberNum= "Members:  2 of 5"></ListItem>
+                <ListItem  index={2} image={lionImg} groupName="Group 2" description= "58% match" memberNum= "Members:  2 of 5"></ListItem>
+                <ListItem  index={3} image={lionImg} groupName="Group 3" description= "25% match" memberNum= "Members:  2 of 5"></ListItem>
+                <ListItem  index={4} image={lionImg} groupName="Group 4" description= "15% match" memberNum= "Members:  2 of 5"></ListItem>
             </div>
             <button class= "Add-button">
                 +
@@ -26,16 +27,19 @@ function GroupSearch(){
 
 export default GroupSearch;
 
-function ListItem ({icon, groupName, description, memberNum}) {
+function ListItem({ image, groupName, description, memberNum }) {
     return (
-        <div className = "list-item">
-            <div className = "list-item-icons">{icon}</div>
-            <div className = "list-item-row">
-                <div className = "list-item-group-name">{groupName} 
-                    <div className="list-item-members">{memberNum}</div>
-                </div>
-                <div className = "list-item-description">{description}</div>
-            </div>
+      <div className="list-item">
+        <div className="list-item-icons">
+          <img src={image} alt="Group Icon" className="profile-image" />
         </div>
+        <div className="list-item-row">
+          <div className="list-item-group-name">
+            {groupName}
+            <div className="list-item-members">{memberNum}</div>
+          </div>
+          <div className="list-item-description">{description}</div>
+        </div>
+      </div>
     );
-}
+  }
