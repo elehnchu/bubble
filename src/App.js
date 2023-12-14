aa948e3175e6f1d4033f0a9c5e5c542b6014d035
Akinfolarin <akin.ogunmodede@gmail.com>
2023-12-13 22:30:18 -0500

    Made create group page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GroupSearch from './components/GroupSearch.js';
 import UserProfile from './components/UserProfile';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import RequestGroup from './components/RequestGroup.js';
+import CreateGroup from './components/CreateGroup.js'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<GroupSearch />} />
           <Route path="/userprofile" element={<UserProfile />} />
           <Route path="/requestgroup" element={<RequestGroup />} />
+          <Route path="/creategroup" element={<CreateGroup />} />
         </Routes>
       </div>
     </Router>
