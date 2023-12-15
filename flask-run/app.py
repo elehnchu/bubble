bd4f5357c3d2f31bb8d6dd556aa26dd8927529c8
Lara Karacasu <larakaracasu@gmail.com>
2023-12-15 16:46:59 -0500

    Fixed npm build bug.

--- flask-run/app.py
+++ flask-run/app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+load_dotenv() # load env variables
+
+import os
+stream_api_key = os.getenv('STREAM_API_KEY')
+stream_api_secret = os.getenv('STREAM_API_SECRET')
+stream_app_id = os.getenv('STREAM_APP_ID')
 
 app = Flask(__name__, template_folder='.')
 
