f2b4f243d5e3c59261320e22ffaa300b28f9a6bb
elehnchu <helenchu90@gmail.com>
2023-12-16 15:36:05 -0500

    Courses & Settings Page finished

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,25 +2,32 @@ import './Courses.css';
 import fishImg from "../images/fish_nobubbles.png"
 import React from "react";
 import fishsmall from "../images/fish_small.png"
-import background_bubble from "../images/bubble_blue.png"
-//import background_bubble from "../images/bubble_blue.png"
-//import background_bubble from "../images/bubble_blue.png"
-//import background_bubble from "../images/bubble_blue.png"
+import bubble_blue from "../images/bubble_blue.png"
+import bubble_purple from "../images/Bubble_purple.png"
+import bubble_green from "../images/Bubble_green.png"
+import bubble_beige from "../images/Bubble_beige.png"
+import Back_arrow from "../images/Backward arrow.png"
+
 
 function Courses(){
     return (
         <div className="Courses_Header">
-            <div className ="Header-courses">
-                Courses
-            </div>
+                <div className ="Header-courses">
+                    <div style={{ backgroundImage:`url(${Back_arrow})` }}className = "Back-arrow">
+                    </div>
+                    <div className ="Header-courses-title">
+                        Courses
+                    </div>
+                </div>
+
             <img className="fish-pic1" src={fishsmall}></img>
             <div className ="Course-container">
             <div className = "List-courses">
-                <ListItem  index={1} courseName="COMS 4111" courseTitle="Introduction to Databases"></ListItem>
-                <ListItem  index={2} courseName="COMS 4170" courseTitle="UI Design"></ListItem>
-                <ListItem  index={3} courseName="APMA 3101" courseTitle="Applied Math I: Linear Algebrea"></ListItem>
-                <ListItem  index={4} courseName="STAT 2102" courseTitle="Applied Statistical Computing"></ListItem>
-                <ListItem  index={5} courseName="COMS 4111" courseTitle="Introduction to Databases"></ListItem>
+                <ListItem  index={1} background_bubble= {bubble_blue} courseName="COMS 4111" courseTitle="Introduction to Databases"></ListItem>
+                <ListItem  index={2} background_bubble= {bubble_purple} courseName="COMS 4170" courseTitle="UI Design"></ListItem>
+                <ListItem  index={3} background_bubble= {bubble_green} courseName="APMA 3101" courseTitle="Applied Math I: Linear Algebrea"></ListItem>
+                <ListItem  index={4} background_bubble= {bubble_beige} courseName="STAT 2102" courseTitle="Applied Statistical Computing"></ListItem>
+                <ListItem  index={5} background_bubble= {bubble_blue} courseName="COMS 4111" courseTitle="Introduction to Databases"></ListItem>
             </div>
             </div>
             <div className = "fish-container">
@@ -35,7 +42,7 @@ function Courses(){
 
 export default Courses;
 
-function ListItem({ index, courseName, courseTitle}) {
+function ListItem({ background_bubble, courseName, courseTitle}) {
     return (
       <div style={{ backgroundImage:`url(${background_bubble})` }} className="list-item-courses">
           <div className="list-item-courses-name">
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,30 +6,36 @@ import NI from "../images/Notifications Icon.png"
 import AUI from "../images/About Us Icon.png"
 import PPI from "../images/Privacy PPolicy Icon.png"
 import HI from "../images/Help Icon.png"
+import Back_arrow from "../images/Backward arrow.png"
+import Right_arrow from "../images/right_arrow.png"
 
 function Settings(){
     return (
         <div className="settings_Header">
             <div className ="Header-settings">
-                Settings
-            </div>
+                    <div style={{ backgroundImage:`url(${Back_arrow})` }}className = "Back-arrow">
+                    </div>
+                    <div className ="Header-settings-title">
+                        Settings
+                    </div>
+                </div>
             <div className ="Settings-container">
                 <div className = "General-title">
                     General
                 </div>
             <div className = "List-general">
-                <ListItem  index={1} image={LPI} Title="Introduction to Databases"></ListItem>
-                <ListItem  index={2} image={ICI} Title="Introduction to Databases"></ListItem>
-                <ListItem  index={3} image={NI} Title="Introduction to Databases"></ListItem>
+                <ListItem  index={1} icon_image={LPI} Title="Liked Profiles"></ListItem>
+                <ListItem  index={2} icon_image={ICI} Title="Import Courses"></ListItem>
+                <ListItem  index={3} icon_image={NI} Title="Notifications"></ListItem>
 
             </div>
                 <div className = "Info-title">
                     Info and Support
                 </div>
             <div className = "List-info">
-                <ListItem  index={1} image={AUI} Title="Introduction to Databases"></ListItem>
-                <ListItem  index={2} image={PPI} Title="Introduction to Databases"></ListItem>
-                <ListItem  index={3} image={HI} Title="Introduction to Databases"></ListItem>
+                <ListItem  index={1} icon_image={AUI} Title="About Us"></ListItem>
+                <ListItem  index={2} icon_image={PPI} Title="Privacy Policy"></ListItem>
+                <ListItem  index={3} icon_image={HI} Title="Help"></ListItem>
             </div>
             </div>
             
@@ -39,12 +45,16 @@ function Settings(){
 
 export default Settings;
 
-function ListItem({image, Title}) {
+function ListItem({icon_image, Title}) {
     return (
       <div className="list-item-settings">
-          <div className="list-item-settings-name">
-            {Title}
-          </div>
+            <div style={{ backgroundImage:`url(${icon_image})` }}className = "icon-image">
+                    </div>
+            <div className="list-item-settings-name">
+                {Title}
+            </div>
+            <div className ="right-arrow">
+            </div>
           </div>
     );
   }
