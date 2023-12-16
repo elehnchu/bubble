2e678c5d6fc114828d36ffe514fa5c1da1bb20c5
Lara Karacasu <larakaracasu@gmail.com>
2023-12-16 03:53:10 -0500

    Basic working text messaging interface on ChatSearch.js

--- src/components/ChatSearch.js
+++ src/components/ChatSearch.js
@@ -4,10 +4,14 @@ import { Chat, Channel, ChannelHeader, MessageList, MessageInput, Thread, Window
 import 'stream-chat-react/dist/css/index.css';
 
 const client = new StreamChat(process.env.REACT_APP_STREAM_API_KEY);
-client.connectUser({ id: '5', name: 'Test' }, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNSJ9.REqW1dxp6rOnIOUZ-Gq9zE4jVVRTOySXcFOmNBVm09I');
+client.connectUser(
+    {id: '5', 
+    name: 'Test'
+},
+client.devToken('5'.toString()));
 
-const channel = client.channel('livestream', 'channelname', {
-    members: ['5'], // Replace with the actual user IDs
+const channel = client.channel('messaging', 'channel-name', {
+    members: ['1', '5'], // Replace with the actual user IDs
     // other channel data...
   });
   
