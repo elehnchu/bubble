b15321d408076ebc4a4e30508434ad00732aff67
Lara Karacasu <larakaracasu@gmail.com>
2023-12-17 18:38:24 -0500

    Finished members list, dynamic user profile info generation

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,69 +1,64 @@
-// UserProfile.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import usersData from '../json/users.json'; // Ensure this path is correct
+import BackArrowImage from '../images/Backward arrow.png';
+import chatImg from '../images/footer_chat.png';
+import classesImg from '../images/footer_classes.png';
+import profileImg from '../images/footer_profile.png';
+import DefaultProfile from '../assets/profile-photos/default.png';
 import './UserProfile.css';
-import './Star.css';
-import Star from '../images/star.jpg'
-import DefaultProfile from '../assets/profile-photos/default.png'
-import { useNavigate } from 'react-router-dom';
-import Back_arrow from "../images/Backward arrow.png"
-import classesImg from '../images/footer_classes.png'
-import profileImg from '../images/footer_profile.png'
-import chatImg from '../images/footer_chat.png'
-
-//el: not sure if this would back to /memberlist, it's not developed atm, so 
-//temp nav is login
 
 const UserProfile = () => {
-
   const navigate = useNavigate();
-    const handleBackClick = () => {
-        navigate(-1)
-    }
-    const handleChatClick = () => {
-      navigate('/channellist') // fix later
-    }
-  
-    const handleClassesClick = () => {
-      navigate('/courses') // fix later
-    }
-  
-    const handleProfileClick = () => {
-      navigate('/yourprofile')
-    }
+  const location = useLocation();
+  const [user, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    const memberId = location.state?.memberId;
+    const userData = usersData.find(user => String(user.id) === String(memberId));
+    setUserProfile(userData);
+  }, [location.state]);
+
+  // Handlers for navigation
+  const handleBackClick = () => navigate(-1);
+  const handleChatClick = () => navigate('/channellist');
+  const handleClassesClick = () => navigate('/courses');
+  const handleProfileClick = () => navigate('/yourprofile');
+
+  if (!user) {
+    return <div>Loading...</div>; // Or some loading indicator
+  }
 
   return (
     <>
-    <div className="Header_user_info">
-      <div style={{ backgroundImage:`url(${Back_arrow})` }}className = "Back-arrow" onClick={handleBackClick}>
-        </div>
-        </div>
-    <div className="user-profile">
-        <img src={DefaultProfile} alt='' className="profile-photo" />
-        <img src={Star} alt='' />
-      <h1 className="user-text">User</h1>
-      <p className="email-text">user1@columbia.edu</p>
+      <div className="Header_user_info">
+        <img src={BackArrowImage} alt="Back" className="Back-arrow" onClick={handleBackClick} />
+      </div>
+      <div className="user-profile">
+        <img src={user.profilePicUrl || DefaultProfile} alt={user.name} className="profile-photo" />
+        <h1 className="user-text">{user ? user.name : "You"}</h1>
+      <p className="email-text">{user ? user.email : "abc123@email.com"}</p>
       <div className="detail-content">
-        <div className="tag">night owl</div>
-        <div className="tag">group learner</div>
-        <div className="tag">coffee lover</div>
-        <div className="tag">multitasker</div>
-        <div className="tag">critical thinker</div>
+        {user ? (user.tags.map((tag) => {
+          return <div className="tag">{tag}</div>
+          }
+        )) : []}
       </div>
       <div className="details">
         <div className="detail-title">Major / Minor:</div>
         <div className="detail-content">
-          <div className="tag">Computer Science</div>
-          <div className="tag">Math</div>
+          <div className="tag">{user ? user.major : ""}</div>
+          <div className="tag">{user ? user.minor : ""}</div>
         </div>
         <div className="detail-title">About Me:</div>
-        <p className="about-me-text">Lorem ipsum text</p>
+        <p className="about-me-text">{user ? user.bio : ""}</p>
       </div>
     </div>
-    <div className="Footer">
-                <img src={chatImg} alt='' className="footer-photo" onClick={handleChatClick}/>
-                <img src={classesImg} alt='' className="footer-photo" onClick={handleClassesClick}/>
-                <img src={profileImg} alt='' className="footer-photo" onClick={handleProfileClick}/>
-            </div>
+      <div className="Footer">
+        <img src={chatImg} alt='' className="footer-photo" onClick={handleChatClick}/>
+        <img src={classesImg} alt='' className="footer-photo" onClick={handleClassesClick}/>
+        <img src={profileImg} alt='' className="footer-photo" onClick={handleProfileClick}/>
+    </div>
     </>
   );
 };
